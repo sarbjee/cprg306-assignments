77cd22e83be38b2c09d1aa2939832f8d8171700b
sarb <sarbjeetb2@gmail.com>
2023-09-23 14:02:40 -0600

    week2 Assignment create

---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import studentInfo from "../studentInfo";
+import Link from "next/link"
+
+export default function Home() {
+    return (
+        <main>
+            <h1>My shopping List</h1>
+            <studentInfo/>
+        </main>
+    )
+}
