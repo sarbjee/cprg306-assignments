df93bcac96e911f6d4c78f5e696ea7fb01589625
sarb <sarbjeetb2@gmail.com>
2023-10-02 12:30:58 -0600

    error finxed week3

--- app/page.js
+++ app/page.js
@@ -7,8 +7,9 @@ export default function Home() {
     <main>
       <h1>CPRG 306 :WEB DEVELOPMENT 2  ASSIGNMENTS</h1>
       <StudentInfo/>
-      <Link href={"/week2"}>week2 Link</Link>
-      <Link className="hover:underline" href={"/week3"}></Link>
+      <Link className="hover:underline" href={"/week2"}>WEEK 2 Link</Link>
+
+      <Link className="hover:underline" href={"/week3"}>WEEK 3 Link</Link>
       
      </main>
     
--- app/week3/page.js
+++ app/week3/page.js
@@ -1,10 +1,12 @@
 import Item from "./item";
 import ItemList from "./item-list";
+import StudentInfo from "./studentInfo";
 
 export default function Page(){
     return(
         <main className="justify-center m-10">
-            <ItemList/>
+            <ItemList />
+            <StudentInfo/>
         </main>
     )
 }
