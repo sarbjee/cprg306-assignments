fada7bbba17716efdf3d3003ac4816b020b53e67
sarb <sarbjeetb2@gmail.com>
2023-10-02 12:50:36 -0600

    week 3

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
-
 import StudentInfo from "./week2/studentInfo";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306 :WEB DEVELOPMENT 2  ASSIGNMENTS</h1>
+      <h1>CPRG 306 :WEB DEVELOPMENT 2 ASSIGNMENTS</h1>
       <StudentInfo/>
-      <Link className="hover:underline" href={"/week2"}>WEEK 2 Link</Link>
-
-      <Link className="hover:underline" href={"/week3"}>WEEK 3 Link</Link>
       
-     </main>
-    
+      <div className="mt-4">
+        <Link className="hover:underline" href={"/week2"}>WEEK 2 Link</Link>
+      </div>
+
+      <div className="mt-4">
+        <Link className="hover:underline" href={"/week3"}>WEEK 3 Link</Link>
+      </div>
+      
+    </main>
   )
 }
+
