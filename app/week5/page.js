f8b8b2b15ae59ecdcc6a130f9a54b312530eeea0
sarb <sarbjeetb2@gmail.com>
2023-10-13 23:08:22 -0600

    week5 new file

---
+++ app/week5/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemList from "./item-list";
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div>
+           
+            <ItemList/>
+          
+        </div>
+    );
+}
